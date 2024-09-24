c8e3b662f2e9b2d8acf1fa834da0bda2447584dd
Kojo Patrick <patrickasare84@gmail.com>
2024-09-24 13:51:47 +0000

    update layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
+      <body className={"grainy min-h-screen font-sans antialiased"}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
