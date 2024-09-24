60199caf3c473d9efbe9ebc7a766f3bc6b96e69e
Kojo Patrick <patrickasare84@gmail.com>
2024-09-24 11:14:42 +0000

    refactor db and schema

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,20 +1,18 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-
+import { Pool } from "@neondatabase/serverless";
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { env } from "@/env";
-import ws from "ws";
 
-neonConfig.webSocketConstructor = ws;
 const connectionString = `${env.DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaNeon(pool),
+    adapter,
   });
 
 const globalForPrisma = globalThis as unknown as {
