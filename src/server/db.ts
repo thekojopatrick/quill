bb4b8a1fe434a1adbd478a789badf3952b29d3fe
Kojo Patrick <patrickasare84@gmail.com>
2024-09-24 10:03:55 +0000

    setup new database

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,20 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+
 import { PrismaClient } from "@prisma/client";
-
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { env } from "@/env";
+import ws from "ws";
 
-const psClient = new Client({ url: env.DATABASE_URL });
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
+    adapter: new PrismaNeon(pool),
   });
 
 const globalForPrisma = globalThis as unknown as {
