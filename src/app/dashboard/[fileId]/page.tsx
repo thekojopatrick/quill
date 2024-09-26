8f20e53dc7b5635f8e20d45da570329790d3126a
Kojo Patrick <patrickasare84@gmail.com>
2024-09-26 17:33:40 +0000

    [wip] upload button

---
+++ src/app/dashboard/[fileId]/page.tsx
@@ -0,0 +1,53 @@
+import { notFound, redirect } from "next/navigation";
+
+import ChatWrapper from "@/components/chat/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
+import { db } from "@/server/db";
+//import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+interface PageProps {
+  params: {
+    fileId: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { fileId } = params;
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user ?? !user?.id) redirect(`/auth-callback?origin=dashboard/${fileId}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound();
+
+  //const plan = await getUserSubscriptionPlan();
+
+  return (
+    <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
+      <div className="max-w-8xl mx-auto w-full grow lg:flex xl:px-2">
+        {/* Left sidebar & main wrapper */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area  */}
+            <PdfRenderer url={file.url} />
+          </div>
+        </div>
+
+        <div className="flex-[0.75] shrink-0 border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper isSubscribed={true} fieldId="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/chat/ChatWrapper.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChatWrapper = ({
+  isSubscribed,
+  fieldId,
+}: {
+  isSubscribed: boolean;
+  fieldId: string;
+}) => {
+  return <div>ChatWrapper</div>;
+};
+
+export default ChatWrapper;
